1f1b7e2c343a0cb53c2ca47c76a6fc9be1189342
chnesi <953060322@qq.com>
2020-06-13 00:50:32 +0800

    登录退出功能的实现

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-row>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
